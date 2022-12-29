cd6ef3d758cf245b79c4eff7521a09c415f2604f
Sergey_Aleksev <2soulhunter@mail.ru>
2022-12-29 13:24:58 +0300

    add .\Z64\Program.cs

---
+++ Z64/Program.cs
@@ -0,0 +1,17 @@
+﻿/*                          Домашнее задание к семинару 9 С#
+Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+Например:
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"               */
+
+Console.Write("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ShowNumFromNto1(N));
+
+
+string ShowNumFromNto1(int N)
+{
+    if (N==1) return 1.ToString();
+    else return ($"{N}, {ShowNumFromNto1(N-1)}");
+}
